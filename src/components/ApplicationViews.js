18f56d9b7afdb18eb343bfc4407b91b5f8193101
Raparker88 <raparker88@gmail.com>
2020-12-17 13:11:16 -0600

    created a view for making a reading with deck and shuffle button displaying and functional

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { UserProvider } from "./users/UserProvider"
 import { Card } from "./home/CardOfDay"
+import { CardProvider } from "./readings/CardProvider"
+import { MakeReading } from "./readings/MakeReading"
 
 
 export const ApplicationViews = () => {
@@ -9,6 +11,10 @@ export const ApplicationViews = () => {
         <UserProvider>
             <Route exact path="/" render={props =>
                 <Card {...props} />} />
+            <CardProvider>
+                <Route exact path="/reading" render={props =>
+                    <MakeReading {...props} />} />
+            </CardProvider>
         </UserProvider>
         
     </>
