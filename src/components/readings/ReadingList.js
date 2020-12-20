e5b2f233fd73589ede5089ed54f1ed33d071a3b6
Raparker88 <raparker88@gmail.com>
2020-12-20 17:25:59 -0600

    checking props to change reading state variable

--- src/components/readings/ReadingList.js
+++ src/components/readings/ReadingList.js
@@ -2,21 +2,20 @@ import React, { useContext, useEffect } from 'react'
 import { ReadingContext } from "./ReadingProvider"
 import { UserContext } from "../users/UserProvider"
 import "./Reading.css";
-import { useParams } from "react-router"
 
 
 export const ReadingList = (props) => {
     const { getReadingsByUser, readings, getSubscriptions } = useContext(ReadingContext)
     const { currentUser, getCurrentUser } = useContext(UserContext)
 
-    let {route}= useParams()
+    let route= props.location.pathname
 
     useEffect(() => {
         getCurrentUser()
     },[]) 
     
     useEffect(() => {
-        if(route="readings"){
+        if(route === "/readings"){
             getSubscriptions()
         }else{
             getReadingsByUser(currentUser.id)
@@ -27,7 +26,7 @@ export const ReadingList = (props) => {
     
     return (
         <>
-            <h2>My Readings</h2>
+            <h2>{route === "/readings"? "Readings": "My Readings"}</h2>
             {readings.map(reading => {
                 return <div key={reading.id} className="reading-list-detail" 
                 onClick={()=> props.history.push(`/readings/${reading.id}`)}>
